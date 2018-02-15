c2c76fe6520bfc905debff6495e86f63047808cb
Nass <mezouar.nass@gmail.com>
2018-02-15 17:33:52 +0100

    mister cocktail day1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [ :index, :new, :create, :show] do
+    resources :doses, only: [ :new, :create]
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
+  root "cocktails#index"
+  resources :doses, only: [ :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'json'
+require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Ingredient.destroy_all
+# Cocktail.destroy_all
+# Dose.destroy_all
+
+# cocktail = Cocktail.create!(name: "mojito")
+# ingredient = Ingredient.create!(name: "lemon")
+
+# dose = Dose.create!(
+#   cocktail: cocktail,
+#   ingredient: ingredient,
+#   name: "blabla",
+#   description: "1"
+# )
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+# puts "#{user}"
+
+ user["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient.values.first)
+end
+#  user["drinks"].each do |ingredient|
+#   p ingredient["strIngredient1"]
+# end
+
+
+# p user["drinks"].each do |element|
+
+#   Ingredient.create(name: element["strIngredient1"])
+# end
